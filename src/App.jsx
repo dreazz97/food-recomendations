e62b1461bf8f077f3d3c5b36920e9cb50e6992f5
Iuri Peniche <iuri@pgtecmed.com>
2023-09-29 11:26:00 +0100

    Switched BrowserRouter for HashRouter so it works with GH-Pages.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import BegPage from './components/BegPage';
 import RecipePage from './components/RecipePage';
 import Grid from '@mui/material/Grid';
