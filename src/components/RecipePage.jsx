80fc25cc97b0ef4d4447ca992acbf2f93407cb6b
Iuri Peniche <iuri@pgtecmed.com>
2023-09-30 16:07:49 +0100

    Two cards per row in large screen sizes.

--- src/components/RecipePage.jsx
+++ src/components/RecipePage.jsx
@@ -30,7 +30,7 @@ const RecipePage = () => {
   return (
     <>
       <Grid container>
-      <Grid item xs={1} sm={2} md={3} lg={4}>
+      <Grid item xs={1} sm={2} md={3} lg={2}>
       </Grid>
         <Grid item xs={1} sm={2}>
         <ArrowBackIosIcon id="Back-arrow" onClick={handleBackClick} />
@@ -39,10 +39,18 @@ const RecipePage = () => {
     {isLoading ? (
       <img id='loading' src={loading} alt="loading" />
     ) : dataRecipes && dataRecipes.length > 0 ? (
-      dataRecipes.map((el, index) => (
-        <RecipeComp key={index} label={el.recipe.label} image={el.recipe.image} calories={el.recipe.calories} source={el.recipe.url} prepartime={el.recipe.totalTime}
-        dietLabels={el.recipe.dietLabels} ingredients={el.recipe.ingredientLines} />
-      ))
+    <div id='main-container'>
+      <Grid container justifyContent="center">
+        {dataRecipes.map((el, index) => (
+          <Grid item xs={12} sm={12} md={12} lg={6}>
+            <div style={{ maxWidth: '345px', margin: 'auto' }}>
+              <RecipeComp key={index} label={el.recipe.label} image={el.recipe.image} calories={el.recipe.calories} source={el.recipe.url} prepartime={el.recipe.totalTime}
+              dietLabels={el.recipe.dietLabels} ingredients={el.recipe.ingredientLines} />
+            </div>
+          </Grid>
+        ))}
+      </Grid>
+    </div>
     ) : (
         <>
         <p id='no-results' className='descriptions'>Oops... Seems like your query didn't match any results</p>
